981be697671d3ee3a9d035cea518a7c17d4174c6
mahabub alam <mahabu998@gmail.com>
2021-10-12 10:18:34 +0600

    sceond add

---
+++ functionTwo.js
@@ -0,0 +1,19 @@
+function doubleIt(num){
+    var result = num*2;
+    // console.log(result);
+    return result;
+}
+var first = doubleIt(5)
+var second =  doubleIt(50)
+var third = doubleIt(100)
+var total = first + second + third
+// console.log(first, second, third)
+// console.log(total)
+
+function add (num1,num2) {
+     
+    var result = num1+num2;
+    return result;
+}
+var sum = add(17,15);
+console.log(sum)
---
+++ object.js
@@ -0,0 +1,15 @@
+var student = {id:121,phoneNumber:0174,name:"mahabub"};
+var student2 = {id:131,phoneNumber:2121}
+
+var phone = student.phoneNumber
+console.log(phone)
+// second way
+var phoneNum = student2["phoneNumber"]
+console.log(phoneNum)
+// update phone number
+student2.phoneNumber = 4585245656
+
+student2.cinema = "smart gril";
+console.log(student);
+console.log(student2);
+
